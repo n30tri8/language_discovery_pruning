35d8fa5631a41e6838d709baf8d7651c06191cdb
n30tri8 <n30tri8@gmail.com>
2026-01-12 11:47:48 +0100

    back to left padding

--- utils.py
+++ utils.py
@@ -42,6 +42,7 @@ def setup_tokenizer(model_name):
                                               )
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.pad_token_id = tokenizer.eos_token_id  # inference-safe
 
     return tokenizer
 
