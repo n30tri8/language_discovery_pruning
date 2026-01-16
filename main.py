d9361af0d11382bc5d6210f2a3d13a4f419a465b
n30tri8 <n30tri8@gmail.com>
2026-01-16 13:58:27 +0100

    debug gpu out of memory: add print statements

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import csv
+import gc
 import os
 from functools import partial
 
@@ -137,8 +138,8 @@ def prune(model_name, sparsity_ratios, run_env, selected_languages, save_pruned_
         del raw_model
 
         for ratio in sparsity_ratios:
-            print(f"debug=calib_data::inp.device={calib_data['inps'][2].device}")
-            print(f"debug=calib_data::attention_masks.device={calib_data['attention_masks'][5].device}")
+            print(f"debug[sparsity_ratios]=calib_data::inp.device={calib_data['inps'][2].device}")
+            print(f"debug[sparsity_ratios]=calib_data::attention_masks.device={calib_data['attention_masks'][5].device}")
             print(f"\n=== Pruning on linguistic benchmark: '{benchmark}', ratio: {ratio} ===")
             model_to_prune = load_raw_model(model_name)
 
@@ -169,6 +170,7 @@ def prune(model_name, sparsity_ratios, run_env, selected_languages, save_pruned_
                 print(f"Delegated saving model to thread: {thread}, save path: {save_path}")
 
             del model_to_prune
+            gc.collect()
             torch.cuda.empty_cache()
 
     fout.close()
