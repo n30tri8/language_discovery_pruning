fff31b0f47ad1fa2e14ec0b2a2d053217b5c0936
n30tri8 <n30tri8@gmail.com>
2026-01-16 17:53:24 +0100

    refine print statement for consistency in pruning output

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ def prune(model_name, sparsity_ratios, run_env, selected_languages, save_pruned_
             # Prune
             with torch.no_grad():
                 prune_wanda(model_to_prune, calib_data, ratio / 100.0)
-            print("\n=== Wanda-based pruning done ===")
+            print("=== Wanda-based pruning done ===")
 
             linguistic_eval = evaluate_on_linguistic(model_to_prune, tokenizer, evaluation_spec)
             # Log post-pruning evaluation for this ratio
